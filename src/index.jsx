5f6c466c7e54958dbf0a2dd3ad9afeb39216b8d2
aryaidnani <idnaniarya@gmail.com>
2025-03-06 23:39:42 +0530

    Fixed paths

--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,28 @@ import ReactDOM from "react-dom/client";
 import "./styles.css";
 
 const recipes = [
-  { name: "Butter Chicken", time: "45 Minutes", photo: "src/assets/img2.jpg" },
-  { name: "Thai Curry", time: "35 Minutes", photo: "src/assets/img3.jpg" },
+  {
+    name: "Butter Chicken",
+    time: "45 Minutes",
+    photo: "./src/assets/img2.jpg",
+  },
+  { name: "Thai Curry", time: "35 Minutes", photo: "./src/assets/img3.jpg" },
   {
     name: "Margherita Pizza",
     time: "20 Minutes",
-    photo: "src/assets/img4.jpg",
+    photo: "./src/assets/img4.jpg",
   },
-  { name: "Focaccia", time: "40 Minutes", photo: "src/assets/img5.jpg" },
-  { name: "Caeser Salad", time: "20 Minutes", photo: "src/assets/img6.jpg" },
-  { name: "Brownies", time: "50 Minutes", photo: "src/assets/img7.jpg" },
-  { name: "Tomato Soup", time: "15 Minutes", photo: "src/assets/img8.jpg" },
-  { name: "Hummus", time: "45 Minutes", photo: "src/assets/img9.jpg" },
-  { name: "Fish Fingers", time: "30 Minutes", photo: "src/assets/img10.jpg" },
-  { name: "Avocado Tacos", time: "45 Minutes", photo: "src/assets/img11.jpg" },
+  { name: "Focaccia", time: "40 Minutes", photo: "./src/assets/img5.jpg" },
+  { name: "Caeser Salad", time: "20 Minutes", photo: "./src/assets/img6.jpg" },
+  { name: "Brownies", time: "50 Minutes", photo: "./src/assets/img7.jpg" },
+  { name: "Tomato Soup", time: "15 Minutes", photo: "./src/assets/img8.jpg" },
+  { name: "Hummus", time: "45 Minutes", photo: "./src/assets/img9.jpg" },
+  { name: "Fish Fingers", time: "30 Minutes", photo: "./src/assets/img10.jpg" },
+  {
+    name: "Avocado Tacos",
+    time: "45 Minutes",
+    photo: "./src/assets/img11.jpg",
+  },
 ];
 
 let likedRecipes = [];
@@ -48,7 +56,7 @@ function Hero() {
   return (
     <div
       className="hero-container"
-      style={{ backgroundImage: `url(${"/src/assets/hero-img.jpg"})` }}
+      style={{ backgroundImage: `url(${"./src/assets/hero-img.jpg"})` }}
     >
       <div className={`header ${scrolled ? "scrolled" : ""}`}>
         <nav className="nav-bar">
